c782a94b5ac0b18d1aa71b795bcc8b8cd1cb8bb6
Robert Dale Smith <robert@robertdalesmith.com>
2019-05-19 17:24:30 -0500

    removes duplicate minified web build

--- web/src/bookmarklet.js
+++
@@ -1,6 +0,0 @@
-var flingPage = function () {
-    var thisUrl = document.location.href;
-    window.open('http://remote.flinger.co/?fling_url=' + thisUrl, 'flinger', 'toolbar=no,width=700,height=570');
-
-};
-flingPage();
